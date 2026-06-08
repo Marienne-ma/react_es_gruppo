d6e59c686b306382c711bedab926c833d569843d
Dade1991 <dade91@msn.com>
2026-06-08 18:08:08 +0200

    FINAL PUSH

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@mantine/core/styles.css'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { MantineProvider } from '@mantine/core'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "@mantine/core/styles.css"
+import "@mantine/notifications/styles.css"
+import "./index.css"
+import App from "./App.jsx"
+import { BrowserRouter } from "react-router-dom"
+import { MantineProvider } from "@mantine/core"
+import { Notifications } from "@mantine/notifications"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <MantineProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </MantineProvider>
-  </StrictMode>,
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <MantineProvider>
+            <Notifications />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </MantineProvider>
+    </StrictMode>,
 )
